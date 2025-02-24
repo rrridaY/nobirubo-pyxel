af207bd45a60043a1f10c95dc0b7cd644d69dc25
kohak_desk <koha.yaspa@icloud.com>
2025-02-24 15:33:37 +0900

    線描画関数用意

---
+++ line.py
@@ -0,0 +1,12 @@
+from Vector2 import Vector2
+import pyxel
+
+# pyxel.line(self.player_x, START_PLAYER_POSY, self.player_x, START_PLAYER_POSY - self.stick_length, 7)
+
+
+def line(start_point : Vector2 , end_point : Vector2, color):
+    """
+    2点間の直線を描画する関数
+    """
+    pyxel.line(start_point.x, start_point.y, end_point.x, end_point.y, color)
+    
