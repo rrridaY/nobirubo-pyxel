6be9f198004e5d87479284af1407cf10a7d6b315
kohak_desk <koha.yaspa@icloud.com>
2025-02-24 15:34:54 +0900

    定数用ファイルの用意

---
+++ constants.py
@@ -0,0 +1,19 @@
+# 定数(スクリーン)
+SCREEN_WIDTH = 160  
+SCREEN_HEIGHT = 120
+
+# 定数(プレイヤー)
+START_PLAYER_POSX = 40
+START_PLAYER_POSY = 90
+
+# 定数(棒)
+STICK_WIDTH = 2
+STICK_DEFAULT_LENGTH = 10    # 棒の初期長さ
+STICK_GROWTH_SPEED = 1       # 棒の伸びる早さ
+STICK_MAX_LENGTH = 90      # 棒の伸び上限
+
+
+
+# シーン
+TITLE_SCENE = "title"
+PLAY_SCENE = "play"
