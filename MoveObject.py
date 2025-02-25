f6d341182be508c423dbf77b647014325db093ab
kohak_desk <koha.yaspa@icloud.com>
2025-02-26 01:35:54 +0900

    移動オブジェクトクラスにfall追加

--- MoveObject.py
+++ MoveObject.py
@@ -1,6 +1,6 @@
 class MoveObject:
     """
-    移動オブジェクトクラス
+    移動オブジェクトクラス(始点、終点を持つ)
     シーンのつなぎ目で動かしたいオブジェクトに継承させる
     """
     def move_left(self, speed):
@@ -9,3 +9,7 @@ class MoveObject:
     def move_right(self, speed):
         self.start_pos.x += speed
         self.end_pos.x += speed
+    def fall(self, speed):
+        self.start_pos.y += speed
+        self.end_pos.y += speed
+    
