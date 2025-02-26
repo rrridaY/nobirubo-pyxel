b49b7ae3a02a5f3323e9d0b6927c0af88a34a748
kohak_desk <koha.yaspa@icloud.com>
2025-02-26 13:51:22 +0900

    つなぎ目なしのゲームがとりあえず完成

--- Scenes/GameScene.py
+++ Scenes/GameScene.py
@@ -9,7 +9,7 @@ from plotTex import plotTexture
 from line import line
 
 # クラスのインポート(データ処理)
-
+import random
 from Texture import Texture # イメージバンクの画像情報を保持するクラス
 from Vector2 import Vector2
 from Player import Player
@@ -30,7 +30,8 @@ class GameScene(BaseScene):
             self,
             current_floor : Floor,
             next_floor : Floor,
-            prev_stick : Stick = Stick(Vector2(0, 90), 0)
+            prev_stick : Stick = Stick(Vector2(0, 90), 0),
+            score: int = 0
             ):
         # プレイヤーの初期位置
         # self.player_pos = Vector2(START_PLAYER_POSX, START_PLAYER_POSY)
@@ -51,12 +52,24 @@ class GameScene(BaseScene):
         # 次の床    
         self.next_floor = next_floor
 
+        # スコア
+        self.score = score
+
+        # GAME OVER描画用
+        self.game_over_flame = 0
+
 
 
     def update(self):
         if pyxel.btnp(pyxel.KEY_R):
             from SceneManager import SceneManager
             SceneManager.change_scene(GameScene())
+
+        # デバッグ用 30フレームごとにプリント ####
+        if pyxel.frame_count % 30 == 0:
+            print("player: ", self.player.pos.x,"next_floor_start: ", self.next_floor.start_pos.x, "next_floor_end: ", self.next_floor.end_pos.x)
+
+        #######################################
 
         # 棒が伸びる前の処理
         if not self.stick.is_length_decided : 
@@ -79,22 +92,47 @@ class GameScene(BaseScene):
         elif self.stick.is_length_decided :
             # プレイヤーが移動中
             if not self.player.is_crossed:
-                self.player.move_right(1)
-                if self.player.pos.x - self.player.size > self.stick.end_pos.x:
+                self.player.move_right(PLAYER_MOVE_SPEED)
+                if self.player.is_x_reached(self.stick.end_pos.x+self.player.size.x):
                     self.player.is_crossed = True
-                    # self.player.pos.x = self.current_floor.end_pos.x
+
+            
             # プレイヤーが移動完了後
-            else:
-                self.player.fall(1)
+            elif self.player.is_crossed:
+                if self.player.is_on_floor(self.next_floor):
+
+                    # プレイヤーが床に乗る
+                    start_floor = Floor(Vector2(START_PLAYER_POSX-10, 90+1), Vector2(START_PLAYER_POSX+10, 90+1))
+                    a = random.randint(start_floor.end_pos.x + 10, 139)
+                    b = random.randint(a, 140)
+                    next_floor = Floor(Vector2(a, 90+1), Vector2(b, 90+1))
+                    from SceneManager import SceneManager
+                    SceneManager.change_scene(GameScene(start_floor, next_floor, score=self.score+1))
 
 
-        # デバッグ　左に動かす
-        # if pyxel.btn(pyxel.KEY_LEFT):
+                else:
+                    # プレイヤーが床から落ちる
+                    self.player.fall(PLAYER_FALL_SPEED)
+                    if self.player.pos.y > SCREEN_HEIGHT:
+                        self.game_over_flame += 1
+                        if self.game_over_flame > 30 * 3:
+                            # ゲーム終了
+                            pyxel.quit()
+
+
+        # デバッグ　左に動かす #################
+        if pyxel.btn(pyxel.KEY_LEFT):
         #     self.stick.move_left(1)
         #     self.current_floor.move_left(1)
         #     self.next_floor.move_left(1)
         #     self.prev_stick.move_left(1)
+            self.player.move_left(1)
+        # デバッグ　右に動かす
+        if pyxel.btn(pyxel.KEY_RIGHT):
+            self.player.move_right(1)
 
+
+        #######################################
 
     def draw(self):
         pyxel.cls(1)
@@ -110,3 +148,14 @@ class GameScene(BaseScene):
         # プレイヤーを描画
         # pyxel.rect(self.player.pos.x-10, self.player.pos.y-10, 10, 10, 7)
         self.player.draw()
+
+
+        # スコアを描画
+        pyxel.text(80, 20, "SCORE: " + str(self.score), 7)
+
+        # playerの座標を描画（点）
+        pyxel.pset(self.player.pos.x, self.player.pos.y, 6)
+
+        # GAME OVERを描画
+        if self.game_over_flame > 0:
+            pyxel.text(60, 60, "GAME OVER", 8)
