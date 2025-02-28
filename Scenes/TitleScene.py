216d791dbdcb91e94e6baa37ebbad6eb3af9653f
kohak_desk <koha.yaspa@icloud.com>
2025-02-28 10:28:38 +0900

    タイトルシーンの設計

--- Scenes/TitleScene.py
+++ Scenes/TitleScene.py
@@ -5,15 +5,25 @@ import pyxel
 from Scenes.BaseScene import BaseScene
 from Scenes.GameScene import GameScene
 
+# クラスのインポート(データ処理)
+
 # 定数
 from constants import *
 
 
 class TitleScene(BaseScene):
+    def __init__(self,score=0):
+        self.score = score
+
+        self.sharebutton = 0# 予定　新しいボタンクラスをtexture継承で作る
+        
+
     def update(self):
         if pyxel.btnp(pyxel.KEY_SPACE):  # スペースキーでゲーム開始
             from SceneManager import SceneManager
             SceneManager.change_scene(GameScene())
+
+        
 
     def draw(self):
         pyxel.cls(0)
