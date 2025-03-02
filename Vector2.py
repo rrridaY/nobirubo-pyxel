22665ab18072fc19f263bfa7141e1fc1f6a2dcd0
kohak_desk <koha.yaspa@icloud.com>
2025-03-02 23:52:49 +0900

    回転に必要な関数追加

--- Vector2.py
+++ Vector2.py
@@ -1,3 +1,6 @@
+import math # 3/2 追加
+
+
 class Vector2:
     def __init__(self, x, y):
         self.x = x
@@ -29,3 +32,23 @@ class Vector2:
 
     def __str__(self):
         return f"({self.x}, {self.y})"
+    
+
+    # ここから回転関連の関数 3/2追加
+    def length(self):
+        return (self.x ** 2 + self.y ** 2) ** 0.5
+    
+    def angle(self):
+        return math.degrees(math.atan2(self.y, self.x))
+    
+    def rotate(self, angle):
+        rad = math.radians(angle)
+        x = self.x * math.cos(rad) - self.y * math.sin(rad)
+        y = self.x * math.sin(rad) + self.y * math.cos(rad)
+        return Vector2(x, y)
+    
+    def moveto(self,other):
+        self.x = other.x
+        self.y = other.y
+        return Vector2(self.x,self.y)
+    ############################
