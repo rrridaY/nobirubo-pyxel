29a8055c5d0519abac01d9a0bad23ae8d4b37a85
kohak_desk <koha.yaspa@icloud.com>
2025-03-25 22:04:25 +0900

    実行時ドット絵のためのimgbankファイル読み込みコード追加

--- main.py
+++ main.py
@@ -13,6 +13,7 @@ from constants import *
 class App:
     def __init__(self):
         pyxel.init(SCREEN_WIDTH,SCREEN_HEIGHT, title="koha game")
+        pyxel.load("my_resource.pyxres")       
         pyxel.run(SceneManager.update, SceneManager.draw)
 
 
